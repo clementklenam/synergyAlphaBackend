01726bb400ba443804064c849792408ba06afbe7
Clement Klenam <62405356+clementklenam@users.noreply.github.com>
2025-02-17 14:29:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.use((err, req, res, next) => {
     });
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Or specify the frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 
 
 
